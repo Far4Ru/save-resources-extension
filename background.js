c417c46177c9fac78ecdcf8b2b4afcb189a3dd28
Far@Ru <faty422@mail.ru>
2025-04-09 13:59:40 +0300

    Added network resource saving

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
 importScripts('jszip.min.js');
+importScripts('buffer@6.0.3.js');
 
 let savedCount = 0;
 const savedSources = new Map();
+const savedNetworkSources = new Map();
 let zip = undefined;
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
@@ -34,18 +36,36 @@ async function startLoad(tabId) {
   await chrome.debugger.attach({ tabId }, "1.3");
   
   await sendDebuggerCommand(tabId, 'Debugger.enable');
+  await sendDebuggerCommand(tabId, 'Network.enable');
 
   chrome.debugger.onEvent.addListener((tab, eventName, script) => {
     
     if (eventName === 'Debugger.scriptParsed' && script.scriptLanguage === 'JavaScript') {
       savedSources.set(script.url,script)
+    } else if (eventName === 'Network.responseReceived') {
+      const { response, requestId, url } = script
+      const result = sendDebuggerCommand(tabId, 'Network.getResponseBody', { requestId })
+      result.then(resBody => {
+        const content = resBody.base64Encoded ? buffer.Buffer.from(resBody.body, 'base64') : resBody.body;
+        savedNetworkSources.set(resolveURLToPath(response.url).path, content)
+      })
     } else {
       console.log(eventName, script)
     }
   })
+
 }
 
 async function endLoad(tabId) {
+  for (const [url, script] of savedNetworkSources) {
+    try {
+        zip.file(url, script);
+    } catch (error) {
+        console.error("Data error:", error);
+        continue
+    } 
+    await new Promise(resolve => setTimeout(resolve, 0));
+  }
     for (const [url, script] of savedSources) {
       try {
         const base64Data =  script.sourceMapURL.split(',')[1];
@@ -97,3 +117,76 @@ function sendDebuggerCommand(tabId, method, params = {}) {
     });
   });
 }
+const resolveURLToPath = (cUrl) => {
+  let filepath, filename, isDataURI;
+  let foundIndex = cUrl.search(/:\/\//);
+  // Check the url whether it is a link or a string of text data
+  if (foundIndex === -1 || foundIndex >= 10) {
+      isDataURI = true;
+      console.log('[DEVTOOL]', 'Data URI Detected!!!!!');
+      // Data URI
+      if (cUrl.indexOf('data:') === 0) {
+          let dataURIInfo = cUrl
+              .split(';')[0]
+              .split(',')[0]
+              .substring(0, 30)
+              .replace(/[^A-Za-z0-9]/g, '.');
+          filename = dataURIInfo + '.' + Math.random().toString(16).substring(2) + '.txt';
+      } else {
+          filename = 'data.' + Math.random().toString(16).substring(2) + '.txt';
+      }
+      filepath = '_DataURI/' + filename;
+  } else {
+      isDataURI = false;
+      if (cUrl.split('://')[0].includes('http')) {
+          // For http:// https://
+          filepath = cUrl.split('://')[1].split('?')[0];
+      } else {
+          // For webpack:// ng:// ftp:// will be webpack--- ng--- ftp---
+          filepath = cUrl.replace('://', '---').split('?')[0];
+      }
+      if (filepath.charAt(filepath.length - 1) === '/') {
+          filepath = filepath + 'index.html';
+      }
+      filename = filepath.substring(filepath.lastIndexOf('/') + 1);
+  }
+
+  // Get Rid of QueryString after ;
+  filename = filename.split(';')[0];
+  filepath = filepath.substring(0, filepath.lastIndexOf('/') + 1) + filename;
+
+  // Remove path violation case
+  filepath = filepath
+      .replace(/:|\\|=|\*|\.$|"|'|\?|~|\||<|>/g, '')
+      .replace(/\/\//g, '/')
+      .replace(/(\s|\.)\//g, '/')
+      .replace(/\/(\s|\.)/g, '/');
+
+  filename = filename.replace(/:|\\|=|\*|\.$|"|'|\?|~|\||<|>/g, '');
+
+  // Decode URI
+  if (filepath.indexOf('%') !== -1) {
+      try {
+          filepath = decodeURIComponent(filepath);
+          filename = decodeURIComponent(filename);
+      } catch (err) {
+          console.log('[DEVTOOL]', err);
+      }
+  }
+
+  // Strip double slashes ---
+  while (filepath.includes('//')) {
+      filepath = filepath.replace('//', '/');
+  }
+
+  // Strip the first slash '/src/...' -> 'src/...'
+  if (filepath.charAt(0) === '/') {
+      filepath = filepath.slice(1);
+  }
+
+  return {
+      path: filepath,
+      name: filename,
+      dataURI: isDataURI && cUrl,
+  };
+};
